9a7e6cd4e88a3dc49c5e85dac8117a41b80d5a48
ShaharHaramatiC <petunia.parpar@gmail.com>
2016-12-07 17:56:33 +0200

    files names fix

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,4 +1,9 @@
 'use strict';
 
 //form data to local storage
-// contactForm
+
+$(document).ready(function() {
+	$('.btn_send').on('click', function(){
+        console.log($('#userMessage').val());
+    });
+});
